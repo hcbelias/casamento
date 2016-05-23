1adad546e53acb2ba2e6e75fa1d29beb62d37c41
henrique elias <hcbe2004@gmail.com>
2016-05-23 20:45:01 -0300

    home page

--- server/config/express.js
+++ server/config/express.js
@@ -49,18 +49,7 @@ export default function(app) {
    * https://github.com/krakenjs/lusca
    */
   if ('test' !== env) {
-    app.use(lusca({
-      csrf: {
-        angular: true
-      },
-      xframe: 'SAMEORIGIN',
-      hsts: {
-        maxAge: 31536000, //1 year, in seconds
-        includeSubDomains: true,
-        preload: true
-      },
-      xssProtection: true
-    }));
+  
   }
 
   app.set('appPath', path.join(config.root, 'client'));
