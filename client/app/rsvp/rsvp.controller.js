347c10fc6763f1311b1eb8604c02bb635b9d686a
henrique elias <hcbe2004@gmail.com>
2016-06-07 23:03:51 -0300

    uploading photos

--- client/app/rsvp/rsvp.controller.js
+++ client/app/rsvp/rsvp.controller.js
@@ -17,7 +17,6 @@
       var ctrlScope = this.scope;
       this.http.post('/api/sendmails', contact).
         then(function() {
-          debugger;
           ctrlScope.successMessage = contact.attend ? 'Sua presença foi confirmada!' :  'Você não confirmou sua presença.' ;
         }, function() {
           ctrlScope.errorMessage = 'Ocorreu um erro ao confirmar sua presença. Favor entrar em contato pelo telefone: (31) 99204 2341';
